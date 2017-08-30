4454efc58ae5fe05f7646e7daba00202c26ee080
Théophile Choutri <theophile@choutri.eu>
2017-08-30 22:42:27 +0200

    better release script, better .gitignore

--- prepare-release.sh
+++ prepare-release.sh
@@ -6,6 +6,8 @@ then
   echo "[!] You really need to give me a version number, you know?"
 else
   mkdir feed-extract-"${1}"
+  stack clean
+  stack build || exit 1
   FEEDEXTRACT_PATH=$(stack path --local-install-root)
   cp LICENSE.md README.md ${FEEDEXTRACT_PATH}/bin/feed-extract feed-extract-"${1}"/
   zip -rv feed-extract-"${1}".zip feed-extract-"${1}"
