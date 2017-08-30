cdbb7f19d4c4792a7eb9eb40ab3e4b66ddb3f27f
Théophile Choutri <theophile@choutri.eu>
2017-08-30 19:48:01 +0200

    outsource the help/version messages to their own module

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,18 @@
 import           Builder
 import           System.Directory   (doesFileExist)
 import           System.Environment
+import qualified Version            as Version
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        []              -> getStdInstead
-        ("-h":_)        -> help
-        ("--help":_)    -> help
-        ("-v":_)        -> version
-        ("--version":_) -> version
+        []              -> putStrLn Version.help
+        ("-":_)         -> getStdInstead
+        ("-h":_)        -> putStrLn Version.help
+        ("--help":_)    -> putStrLn Version.help
+        ("-v":_)        -> putStrLn Version.num
+        ("--version":_) -> putStrLn Version.num
         (path:_) -> do
             doesIt <- doesFileExist path
             if doesIt then do
@@ -18,11 +20,6 @@ main = do
             else
                 putStrLn "[!] Nah, you really need to give a *real* path :/"
 
-help :: IO ()
-help = putStrLn "Usage: feed-extract PATH_TO_DATABASE"
-
-version :: IO ()
-version = putStrLn "Feed-extract v0.0.2"
 
 getStdInstead :: IO ()
 getStdInstead = do
